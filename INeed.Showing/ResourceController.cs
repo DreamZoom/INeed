74f9fc5588c55f10f2a2b9695cb0501d48e94788
王小龙 <1036199213@qq.com>
2017-03-19 23:42:03 +0800

    上传资源

--- INeed.Showing/ResourceController.cs
+++ INeed.Showing/ResourceController.cs
@@ -6,9 +6,52 @@ using System.Threading.Tasks;
 using INeed.Core;
 using INeed.Showing.Models;
 using INeed.Showing.Services;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
 namespace INeed.Showing
 {
     public class ResourceController : ManageControllerBase<ResourceService, Resource>
     {
+        public ActionResult UploadResource(string res_type)
+        {
+            string resource_path = Server.MapPath("/resources");
+            if (!Directory.Exists(resource_path))
+            {
+                Directory.CreateDirectory(resource_path);
+            }
+            string media_resource_path = string.Format("{0}/{1}", resource_path, res_type);
+            if (!Directory.Exists(media_resource_path))
+            {
+                Directory.CreateDirectory(media_resource_path);
+            }
+
+            try
+            {
+                HttpPostedFileBase post_file = Request.Files["resource"];
+                string file_name = string.Format("{0}.{1}", DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss-ffff"), post_file.FileName.Substring(post_file.FileName.LastIndexOf(".")));
+                post_file.SaveAs(media_resource_path + file_name);
+
+                string resource_url = string.Format("/resources/{0}/{1}", res_type, file_name);
+
+                var resource = new Resource()
+                {
+                    ResName = post_file.FileName,
+                    ResContent = resource_url,
+                    IsPrivate = false,
+                    CreateTime = DateTime.Now,
+                    ResOwner = "system",
+                    ResType = res_type
+                };
+                Service.Create(resource);
+
+                return JsonSuccess("上传成功", resource);
+            }
+            catch
+            {
+                return JsonError();
+            }
+
+        }
     }
 }
